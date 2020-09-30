38a62c069a22d45efbbc469f196850cada6d9d82
Tatsuhiro Tsuchiya <t-tutiya@ist.osaka-u.ac.jp>
2020-09-30 12:52:11 +0900

    upload all old files

---
+++ en/merge.pl
@@ -0,0 +1,37 @@
+﻿#perl
+# usage: perl merge.pl filename
+#   filename.txt is the body that will be embedded into template.html.
+# 日本語へん！
+# 機能．template.html の <--include-->の部分に，inputfileを挿入する
+use warnings;
+# use strict;
+use utf8;
+use Encode;
+
+if ($#ARGV < 0) {
+  exit(1);
+}
+
+my $templatefile = "template.html";
+my $inputfile = $ARGV[0] . ".txt";
+my $outputfile = $ARGV[0] . ".html";
+
+open(IN1, $templatefile); 
+open(IN2, $inputfile);
+open(OUT, "> " . $outputfile);
+
+while (my $l1 = <IN1>) {
+  if ($l1 =~ /\s*<--include-->\s*/) {
+    while (my $l2 = <IN2>) {
+      $l2 = Encode::decode("shiftjis", $l2); 
+      print $l2; 
+      print(OUT $l2);
+    }
+  }
+  else {
+    print(OUT $l1);
+  }
+}
+close(IN1);
+close(IN2);
+close(OUT);
---
+++ jp/README.txt
@@ -0,0 +1,6 @@
+ページのつくりかた 2016.08.09
+
+1. index.txt など，もとになるtextファイルを編集する
+
+2. perl merge.pl index
+を実行．index.htmlが作成される
